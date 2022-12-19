9bd305bb1196d263d95924d04b534f50d382efea
egor-sudko <egor.sudko@gmail.com>
2022-12-19 22:16:36 +0300

    League window started

--- FallGuys/Assets/Scripts/Lobby/UI/LeagueLevelVisualizer.cs
+++ FallGuys/Assets/Scripts/Lobby/UI/LeagueLevelVisualizer.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LeagueLevelVisualizer : MonoBehaviour
 {
-    private TextMeshProUGUI leagueText;
+    [SerializeField] private List<Sprite> leagueIcons;
+
+    private int leaguLevel;
+    private Image _image;
 
     private void Awake()
     {
-        leagueText = GetComponent<TextMeshProUGUI>();
+        _image = GetComponent<Image>();
     }
 
     private void UpdateLeagueLevelValue(int value)
     {
-        leagueText.text = value.ToString();
+        leaguLevel = value;
+
+        if (leaguLevel <= leagueIcons.Count) _image.sprite = leagueIcons[leaguLevel - 1];
+        else Debug.LogError("League level higher than shield icons");
     }
 
 

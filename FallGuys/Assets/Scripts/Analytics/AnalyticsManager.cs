9993d88769124f712505cc556604b7fd2896f109
egor-sudko <egor.sudko@gmail.com>
2023-01-14 16:14:15 +0300

    Added Facebook SDK
    Добавил Facebook SDK и сделал его инициализацию при запуске.

---
+++ FallGuys/Assets/Scripts/Analytics/AnalyticsManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnalyticsManager : MonoBehaviour
+{
+    private List<IAnalytics> analytics = new List<IAnalytics>
+    {
+        new FacebookAnalytics()
+    };
+
+    public static AnalyticsManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        foreach (var item in analytics)
+        {
+            try
+            {
+                item.Initialize();
+                Debug.Log($"<color=green>[Analytics]</color> {item.GetName()} Initialized");
+            }
+            catch
+            {
+                Debug.LogError($"<color=red>[Analytics]</color> {item.GetName()} - Failed to Initialize");
+            }
+        }
+    }
+}
---
+++ FallGuys/Assets/Scripts/Analytics/IAnalytics.cs
@@ -0,0 +1,5 @@
+public interface IAnalytics
+{
+    public string GetName();
+    public void Initialize();
+}

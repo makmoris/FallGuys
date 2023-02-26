db510e00bd8baea4ca3371fc17e75cd85574ded6
egor-sudko <egor.sudko@gmail.com>
2023-02-26 18:56:49 +0300

    Update Shield script for shield sound
    Убрал воспроизведение эффекта щита из скрипта. Теперь звук по галочке PlayOnAwake

--- FallGuys/Assets/Scripts/Battle/Shield.cs
+++ FallGuys/Assets/Scripts/Battle/Shield.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class Shield : MonoBehaviour
 {
-    private AudioSource audioSource;
+    //private AudioSource audioSource;
 
     private void Awake()
     {
         if (!gameObject.activeSelf) gameObject.SetActive(true);
 
-        audioSource = GetComponent<AudioSource>();
+        //audioSource = GetComponent<AudioSource>();
     }
 
     private void OnEnable()
     {
-        audioSource.Play();
+       // if(audioSource == null) audioSource = GetComponent<AudioSource>();
+
+       // audioSource.Play();
     }
 }

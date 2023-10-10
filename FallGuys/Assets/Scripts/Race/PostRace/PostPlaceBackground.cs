d635b11520178e47654b3cf37767ac2b6180ec70
egor-sudko <egor.sudko@gmail.com>
2023-10-10 13:22:18 +0300

    Added postplace background change

---
+++ FallGuys/Assets/Scripts/Race/PostRace/PostPlaceBackground.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostPlaceBackground : MonoBehaviour
+{
+    public enum BackgroundChangeVariants
+    {
+        random,
+        queue
+    }
+
+    [SerializeField] private BackgroundChangeVariants backgroundChangeVariant;
+    [Space]
+    [SerializeField] private List<GameObject> backgrounds;
+
+    private const string key = "PostPlaceBackgroundVariantIndex";
+
+    private void OnEnable()
+    {
+        ChangeBackground();
+    }
+
+    private void ChangeBackground()
+    {
+        HideAll();
+
+        switch (backgroundChangeVariant)
+        {
+            case BackgroundChangeVariants.random:
+
+                int r = Random.Range(0, backgrounds.Count);
+                ShowBackground(backgrounds[r]);
+
+                break;
+            case BackgroundChangeVariants.queue:
+
+                int i = PlayerPrefs.GetInt(key, 0);
+                ShowBackground(backgrounds[i]);
+                i++;
+                PlayerPrefs.SetInt(key, i);
+
+                break;
+        }
+    }
+
+    private void ShowBackground(GameObject bacgroundGO)
+    {
+        bacgroundGO.SetActive(true);
+    }
+
+    private void HideAll()
+    {
+        foreach (var back in backgrounds)
+        {
+            back.SetActive(false);
+        }
+    }
+}
